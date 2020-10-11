2e92ae8572641334ece32605d7208daaf552e1f7
Vitalii-Kutsuruk <kutsurukvet@gmail.com>
2020-10-11 22:40:10 +0300

    Added components with and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import TodoList from './Todo/TodoList'
 
+
+const todos = [
+  {id: 1, complited: false, title: 'Buy PS5'},
+  {id: 2, complited: true, title: 'Buy BMW 328'},
+  {id: 3, complited: true, title: 'Walk with dog'}
+]
 
 function App() {
-  return <div></div>;
+  return (
+    <div className='wrapper'>
+      <h1>React tutorial</h1>
+
+      <TodoList todos={todos} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const TodoItem = () => {
+    return(
+        <li>Todo</li>
+    );
+}
+
+
+export default TodoItem;
